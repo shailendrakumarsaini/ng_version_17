8f3bd8c0c0db90efe4cd27ab146676ae994908a3
Shailendra Kumar Saini <sainishailendra1996@gmail.com>
2024-10-07 14:01:14 +0530

    feat: subject concept

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,5 +1,6 @@
 import { Routes } from '@angular/router';
 
 export const routes: Routes = [
-    { path : 'signal', loadComponent : ()=> import('./signal/signal.component').then(c => c.SignalComponent)}
+    { path : 'signal', loadComponent : ()=> import('./signal/signal.component').then(c => c.SignalComponent) },
+    { path : 'subject', loadComponent : ()=> import ('./subject/subject.component').then(c => c.SubjectComponent) }
 ];
