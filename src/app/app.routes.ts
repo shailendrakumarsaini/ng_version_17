277c8777a993dfe07d31ddddde710835a2b85f45
Shailendra Kumar Saini <sainishailendra1996@gmail.com>
2024-11-19 10:34:17 +0530

    feat: currency converter component added

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,11 @@
 import { Routes } from '@angular/router';
 import { TeamComponent } from './team/team.component';
 import { CountryFilterComponent } from './country-filter/country-filter.component';
+import { CurrencyConverterComponent } from './currency-converter/currency-converter.component';
 
 export const routes: Routes = [
     { path: '', redirectTo: 'team', pathMatch: 'full' },
     { path: 'country-filter', component: CountryFilterComponent },
     { path: 'team', component: TeamComponent },
+    { path: 'currency-converter', component: CurrencyConverterComponent },
 ];
---
+++ src/app/currency-converter/currency-converter.component.ts
@@ -0,0 +1,36 @@
+import { NgFor } from '@angular/common';
+import { Component } from '@angular/core';
+import { ApiService } from '../services/api.service';
+
+@Component({
+  selector: 'app-currency-converter',
+  standalone: true,
+  imports: [NgFor],
+  templateUrl: './currency-converter.component.html',
+  styleUrl: './currency-converter.component.scss'
+})
+export class CurrencyConverterComponent {
+  resultText: string = '1 USD = 1 USD';
+  currencyCodes: any;
+
+  constructor(private apiService: ApiService) { }
+
+  ngOnInit() {
+    this.currencyCodes = [
+      { name: "USA", code: "USD" }, 
+      { name: "EURO", code: "EUR" }, 
+      { name: "GBBB", code: "GBP" }, 
+      { name: "Japan", code: "JPY" }, 
+      { name: "Newzeland", code: "NGN" }
+    ]
+  }
+
+  convertToRate(amount: any, convertFrom: any, convertTo: any) {
+    let result = 0;
+    let response = this.apiService.getRates(convertFrom);
+    console.log(response);
+    result = parseFloat(response.rates[convertTo].toFixed(2));
+    console.log(result);
+    this.resultText = `${amount} ${convertFrom} = ${result} ${convertTo}`;
+  }
+}
