3fa50be43076621c061128dae67038ce2fc2d873
Shailendra Kumar Saini <sainishailendra1996@gmail.com>
2025-02-14 19:44:34 +0530

    fix: fixed error

--- src/app/template-driven-form/template-driven-form.component.ts
+++ src/app/template-driven-form/template-driven-form.component.ts
@@ -1,11 +1,11 @@
-import { JsonPipe } from '@angular/common';
+import { CommonModule, JsonPipe } from '@angular/common';
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-template-driven-form',
   standalone: true,
-  imports: [FormsModule, JsonPipe],
+  imports: [FormsModule, JsonPipe, CommonModule],
   templateUrl: './template-driven-form.component.html',
   styleUrl: './template-driven-form.component.scss'
 })
